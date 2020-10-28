e116a59a4da1ca1044d2d5525935c31dcd2e99a4
deemak <deema_k@mail.ru>
2020-10-28 18:30:56 +0300

    Translate locals.ts

---
+++ src/app/@core/local/locals.ts
@@ -0,0 +1,29 @@
+export class Local {
+    static en() {
+        return {
+            'New password info': 'New password info',
+            Password: 'Password',
+            Search: 'Search',
+            Add: 'Add',
+            Load: 'Load',
+            Save: 'Save',
+            Comment: 'Comment',
+            Login: 'Login',
+            Site: 'Site',
+        }
+    }
+
+    static ru() {
+        return {
+            'New password info': 'Новая информация',
+            Password: 'Пароль',
+            Search: 'Поиск',
+            Add: 'Добавить',
+            Load: 'Загрузить',
+            Save: 'Сохранить',
+            Comment: 'Коментарий',
+            Login: 'Логин',
+            Site: 'Сайт',
+        }
+    }
+}
